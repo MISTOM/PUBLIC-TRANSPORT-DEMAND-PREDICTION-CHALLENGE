fd1e60157d478643e44860876fed4bd563f9d9df
MISTOM <kigardetom2001@gmail.com>
2022-05-09 23:26:24 +0300

    find probability passanger will take shuttle from Kijauri

--- Challenge_2.js
+++ Challenge_2.js
@@ -14,12 +14,26 @@ const toEpochTime = (time) => {
     return time_a.getTime()
 }
 
+/**
+ * @param {number} no_of_outcome
+ * @param {number} total_no_of_outcome
+ * @returns {number} probability percentage
+ */
+const probability = (no_of_outcome, total_no_of_outcome) => {
+    return Math.round((no_of_outcome/total_no_of_outcome) * 100)
+}
+
 fs.createReadStream('train_revised.csv').pipe(csv())
     .on('data', (data) => content.push(data) )
     .on('end', () => {
-        const KijauriRoutes = content.filter((el) => {
-            return el.travel_from === 'Kijauri'
-                && toEpochTime(el.travel_time) < toEpochTime('7:30')
+        const kijauri_Routes =
+        content.filter(el => { return el.travel_from === 'Kijauri'
+            && toEpochTime(el.travel_time) < toEpochTime('7:30')
         })
-        // console.log(KijauriRoutes)
+        const no_of_kijauri_routes = kijauri_Routes.length
+        const no_of_kijauri_shuttle_routes =
+            kijauri_Routes.filter(el => el.car_type == 'shuttle').length
+            
+        console.log(`The Probability of a passenger travelling from Kijauri will take a shuttle and depart before 7:30 is ${probability(no_of_kijauri_shuttle_routes, no_of_kijauri_routes)}%`)
+        
     })
